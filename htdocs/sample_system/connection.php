47799759fee261ae321556d1a8f90020bd8157ea
Patrick Jan Gitungo <100455454+patrickgitungo18@users.noreply.github.com>
2024-10-03 00:45:44 +0000

    adding connection database code

--- htdocs/sample_system/connection.php
+++ htdocs/sample_system/connection.php
@@ -1,7 +1,18 @@
 <?php
 
+    $server = "127.0.0.1";
     $username = "mariadb";
     $password = "mariadb";
+    $dbname = "mariadb";
+
+    $conn = mysqli_connect($server, $username, $password, $dbname);
+
+    //check the connection if successfull
+    if (!$conn){
+        die ("connection failed: ". mysqli_connect_error());
+    }else{
+        echo "database connected successfully!";
+    }
 
 
 ?>
